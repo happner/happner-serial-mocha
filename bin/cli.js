8c2527e8f7c18339287d39eb6a488490a7873b9f
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-12-23 08:05:26 +0200

    version 1.5.0

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,35 @@
-#!/usr/bin/env node
+var SerialMocha = require('../lib/serialMocha')
+	, colors = require("colors/safe")
+	, commander = require('commander')
+	, path = require('path')
+	;
 
-var argv=require("minimist")(process.argv.splice(2));
-var tasks=argv._;
-var reporter=argv.reporter;
-var savedata=argv.sm;
-var sh=require("../index.js");
+var smInstance = new SerialMocha();
 
-sh.runTasks(tasks,reporter,savedata)
-	.then(function(results){})
-	.catch(function(error){console.log(error,error.stack)});
+commander
+
+	.version (JSON.parse(require('fs').readFileSync(path.resolve(__dirname, '..','package.json'))).version)
+
+	.option ("-f, --folder <val>", "the folder")
+
+	.parse(process.argv);
+
+var testFolder = commander.option('f').folder;
+
+if (testFolder) {
+
+	if (!path.isAbsolute(testFolder)) testFolder = path.resolve(__dirname, testFolder);
+
+	return smInstance.runTasks(testFolder)
+
+		.then(function (results) {
+			console.log(colors.green("\r\nrun ok, duration: " + results.aggregated.duration));
+		})
+
+		.catch(function (err) {
+			console.log(err, err.stack)
+		});
+}
+
+console.log('run failed: you need to configure a folder containing your test files to do a run');
 
--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-var argv = require("minimist")(process.argv)
-	, SerialMocha = require('./lib/serialMocha')
-	, colors = require("colors/safe")
-;
+var SerialMocha = require('./lib/serialMocha')
+	;
 
-if ((argv._.length > 1) && argv._[1].indexOf("index.js") !== -1) {
-
-	var instanceSerialMocha = new SerialMocha();
-
-	instanceSerialMocha.runTasks()
-		.then(function(results){
-			console.log(colors.green("\r\nrun ok, duration: " + results.aggregated.duration ));
-		})
-		.catch(function(err){console.log(err, err.stack)});
-}
+module.exports = new SerialMocha();
